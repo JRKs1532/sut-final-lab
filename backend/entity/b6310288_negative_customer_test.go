3212b709ebbdfa7c3038a6d48afc91d98fa693d8
JRKs1532 <jeerapat1532@gmail.com>
2023-02-15 10:20:36 -0800

    add Validate b6310288_negative_customer_test.go -close #4

---
+++ backend/entity/b6310288_negative_customer_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func TestNegativeCustomerID(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("Check Negative CustomerID", func(t *testing.T) {
+
+		c := Customer{
+			Name:       "Jee",
+			Email:      "jee@gmail.com",
+			CustomerID: "M1234567",
+		}
+
+		ok, err := govalidator.ValidateStruct(c)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("CustomerID ไม่ถูกต้อง"))
+	})
+}
